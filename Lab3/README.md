5e01656eac65c2850dbba4dbed42b2366c4e2554
SeemaMst <79608619+SeemaMst@users.noreply.github.com>
2023-03-01 19:02:59 -0700

    Cleanup
    Cleaning up code

--- Lab3/README.md
+++ Lab3/README.md
@@ -9,10 +9,24 @@ Use the command prompt and run: set FLASK_APP=application.py
 
 Type: flask run
 
+## Collaborators
+The collaborators for this project are:
+
+Seema Mustaqeem
+
+Luc Nicolet
+
 ## References
 The dataset used for this dataset was the Open Calgary Building Permits dataset:
+
 https://data.calgary.ca/Business-and-Economic-Activity/Building-Permits/c2es-76ed
 
 This app was created with the help of a few leaflet plugins, listed as links below:
+
 https://github.com/jawj/OverlappingMarkerSpiderfier-Leaflet
+
 https://github.com/Leaflet/Leaflet.markercluster
+
+The date range widget used is described here:
+
+https://htmlstream.com/preview/nova-v1.2/documentation/libraries/date-range-picker.html#how-to-use
--- Lab3/application.py
+++ Lab3/application.py
@@ -1,14 +1,8 @@
-import os
-import requests
-import geojson as gpd 
-
 from flask import Flask, session, render_template, request, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,8 +10,6 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 # Landing page - Website login
 @app.route("/", methods=["POST", "GET"])
@@ -33,11 +25,10 @@ def login():
         enddatesplit=enddate.split("/")
         startdate = startdatesplit[2] + "-" + startdatesplit[0] + "-" + startdatesplit[1]
         enddate = enddatesplit[2] + "-" + enddatesplit[0] + "-" + enddatesplit[1]
-        print(startdate, enddate)
        
         geojson_url="https://data.calgary.ca/resource/c2es-76ed.geojson?$where=issueddate between'"
         geojson_url= geojson_url+ startdate + "'and '" + enddate + "'"
 
-        return render_template("index.html", add=geojson_url, teststring=teststring) # , add=geojson_url, fromdate=fromdate, todate=todate)
+        return render_template("index.html", add=geojson_url, teststring=teststring)
         
     return render_template("index.html", add=geojson_url)
